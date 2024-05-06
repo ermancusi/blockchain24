8e7eb09bb9079db581b27d96c196966920c0fc6d
Ernesto Mancusi <e.mancusi2@studenti.unisa.it>
2024-05-06 20:26:49 +0200

    Ex4 Skeleton Added

---
+++ Exercises/Ex4/07-dAppPyTEAL/Accounts/create_account.py
@@ -0,0 +1,25 @@
+from algosdk import account, mnemonic
+import os
+from algosdk import account, mnemonic
+
+
+if __name__=='__main__':
+    accountNames=["Alice","Bob","Charlie","Frank"]
+
+    for accountName in accountNames:
+        privateKey, address = account.generate_account()
+
+        if not os.path.exists(accountName):
+            os.makedirs(accountName)
+
+        with open(accountName+"/"+accountName+".addr",'w') as f:
+            f.write(address)
+            
+        with open(accountName+"/"+accountName+".mnem",'w') as f:
+            f.write(mnemonic.from_private_key(privateKey))
+
+
+    print("Addresses Created!\n")
+
+        
+
